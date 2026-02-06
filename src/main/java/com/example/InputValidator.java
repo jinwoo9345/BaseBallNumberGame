64dc9e6a98b7897f78df45e7f27872e26deb51f1
jinwoo9345 <jjw9345@gmail.com>
2026-02-06 12:10:01 +0900

    Feat: 숫자 입력 기능

---
+++ src/main/java/com/example/InputValidator.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class InputValidator {
+
+    public boolean validate(String number) {
+        Set<Character> set = new HashSet<Character>();
+        // 먼저 네자리인지, 네자리 모두 숫자인지
+        if (number.length() != 4 || !number.matches("[0-9]+")) {
+            return false;
+        }
+        // 이후 Set에 저장하여 중복된 문자 제거
+        for (int i = 0; i < 4; i++) {
+            set.add(number.charAt(i));
+        }
+
+        // 4개 보다 줄었을시 중복된 문자 존재
+        if (set.size() != 4) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/org/example/InputValidatorTest.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import com.example.InputValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+public class InputValidatorTest {
+
+    @DisplayName("입력된 문자는 숫자이다")
+    @Test
+    void testIsNumber(){
+        InputValidator input = new InputValidator();
+
+        boolean result = input.validate("abcd");
+
+        assertThat(result).isFalse();
+    }
+
+
+    @DisplayName("입력된 숫자는 4자리여야한다")
+    @Test
+    void testInputValidator() {
+        InputValidator input = new InputValidator();
+
+        boolean result = input.validate("1234");
+
+        assertThat(result).isTrue();
+
+    }
+
+    @DisplayName("입력된 숫자는 서로다른 4자리 숫자가 아니면 실패")
+    @Test
+    void testInputValidator2() {
+        InputValidator input = new InputValidator();
+
+        boolean result = input.validate("1224");
+
+        assertThat(result).isFalse();
+
+    }
+
+
+    @DisplayName("입력된 숫자는 서로다른 4자리 숫자면 성공")
+    @Test
+    void testInputValidator3() {
+        InputValidator input = new InputValidator();
+
+        boolean result = input.validate("1234");
+
+        assertThat(result).isTrue();
+
+    }
+
+
+}
